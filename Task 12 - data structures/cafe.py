098cede6dbd2ad82252085531d02f3e81b52e41e
Geoffrey Roe <groe181@gmail.com>
2023-12-23 08:51:32 +0000

    Capitalise constants

--- Task 12 - data structures/cafe.py
+++ Task 12 - data structures/cafe.py
@@ -8,16 +8,16 @@
 #Print this cost back to the user.
 
 
-menu = ["coffee", "tea", "scone", "toastie"]
+MENU = ["coffee", "tea", "scone", "toastie"]
 
-stock = {
+STOCK = {
     'coffee': 18,
     'tea': 22,
     'scone': 5,
     'toastie': 9
 }
 
-prices = {
+PRICES = {
     'coffee': 4,
     'tea': 3,
     'scone': 5,
@@ -25,7 +25,7 @@ prices = {
 }
 
 total_cost = 0
-for item in menu:
-    total_cost += stock[item] * prices[item]
+for item in MENU:
+    total_cost += STOCK[item] * PRICES[item]
 
 print(f"The total cost is: £{total_cost}")
